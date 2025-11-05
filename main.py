72f7bd509d9a6e2d3f39f8af36d06b1465c3d46e
BananaKing1 <fasai2549natalia2006@gmail.com>
2025-11-05 10:21:47 +0100

    dfsfs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import button
+from boss import Boss, Beam, Asteroid, Infinite_Background 
 
 pygame.init()
 #create display window
@@ -7,8 +8,8 @@ info=pygame.display.Info()
 SCREEN_HEIGHT = info.current_h
 SCREEN_WIDTH = info.current_w
 
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Buttons')
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
+pygame.display.set_caption('Our game')
 
 #load button images
 start_img = pygame.image.load('sprites/start_btn.png').convert_alpha()
@@ -20,16 +21,31 @@ button_y= SCREEN_HEIGHT/2
 start_button = button.Button(SCREEN_WIDTH/2 +100, button_y, start_img)
 exit_button = button.Button(SCREEN_HEIGHT/2 -100, button_y, exit_img)
 
+clock = pygame.time.Clock()
+
+#load image
+background = Infinite_Background(SCREEN_WIDTH, SCREEN_HEIGHT, "backgrounds/boss_bg_img.png")
+
 #game loop
 run = True
 while run:
+	clock.tick(60) #tick speed
 
+	for event in pygame.event.get():
+		if event.type == pygame.QUIT:
+			run = False
+	
 	screen.fill((0,0,0))
+
+    # Draw scrolling background
+	background.draw(screen, scroll_speed=5)
 
 	if start_button.draw(screen):
 		print('START')
+
 	if exit_button.draw(screen):
 		print('EXIT')
+		run = False
 
 	#event handler
 	for event in pygame.event.get():
