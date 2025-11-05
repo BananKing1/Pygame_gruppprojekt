84f9eb1eca2fb3b86cb37cc517eb878f64adee92
BananaKing1 <fasai2549natalia2006@gmail.com>
2025-11-05 10:07:23 +0100

    fdsf

--- main.py
+++ main.py
@@ -1,15 +1,46 @@
 import pygame
-from button import Button
-
+import button
 
 pygame.init()
-
-# Screen
-info = pygame.display.Info()
-SCREEN_WIDTH  = info.current_w
+#create display window
+info=pygame.display.Info()
 SCREEN_HEIGHT = info.current_h
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT), pygame.FULLSCREEN)
-pygame.display.set_caption("Boss Test")
+SCREEN_WIDTH = info.current_w
 
-clock = pygame.time.Clock()
-running = True
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Buttons')
+
+#load button images
+start_img = pygame.image.load('sprites/start_btn.png').convert_alpha()
+exit_img = pygame.image.load('sprites/exit_btn.png').convert_alpha()
+
+button_y= SCREEN_HEIGHT/2
+
+#create button instances
+start_button = button.Button(SCREEN_WIDTH/2 +100, button_y, start_img)
+exit_button = button.Button(SCREEN_HEIGHT/2 -100, button_y, exit_img)
+
+#game loop
+run = True
+while run:
+
+	screen.fill((0,0,0))
+
+	if start_button.draw(screen):
+		print('START')
+	if exit_button.draw(screen):
+		print('EXIT')
+
+	#event handler
+	for event in pygame.event.get():
+		#quit game
+		if event.type == pygame.QUIT:
+			run = False
+
+	if pygame.key.get_pressed()[pygame.K_ESCAPE]:
+		run =False
+
+	pygame.display.update()
+
+pygame.quit()
+
